e9593812b34518c79e692a6b8bb4fe2bad4c5d3f
Yu Xuan <yuxuanteea02@gmail.com>
2024-05-18 12:02:54 +0800

    update intents.json and data.json, and display image for certain responses

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,7 @@
 # Chatbot implementation (and UI)
 import json
 import streamlit as st
+from PIL import Image
 from response import generate_response
 
 @st.cache_data
@@ -11,12 +12,19 @@ def load_intents():
 
 intents = load_intents()
 
+st.title("UM Shuttle Bus Chatbot")
+st.button("Reset chat", on_click=lambda: st.session_state.pop("messages", None))
+
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
+        if "image" in message and message["image"] is not None:
+            image = message["image"]
+            displayed_image = Image.open(image)
+            st.image(displayed_image)
 
 if prompt := st.chat_input("Ask me something..."):
     with st.chat_message("user"):
@@ -24,9 +32,10 @@ if prompt := st.chat_input("Ask me something..."):
     st.session_state.messages.append({"role": "user", "content": prompt})
 
     response, image = generate_response(prompt, 0.8, intents)
-    # if image is not None:
-    #     response = response + "image"
 
     with st.chat_message("assistant"):
         st.markdown(response)
-    st.session_state.messages.append({"role": "assistant", "content": response})
+        if image is not None:
+            displayed_image = Image.open(image)
+            st.image(displayed_image)
+    st.session_state.messages.append({"role": "assistant", "content": response, "image": image})

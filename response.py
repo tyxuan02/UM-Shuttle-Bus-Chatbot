734b844b64b8ea3095eaae0211314d56551023af
Yu Xuan <yuxuanteea02@gmail.com>
2024-05-27 15:13:54 +0800

    change match/case to if/else

--- response.py
+++ response.py
@@ -12,95 +12,94 @@ def generate_response(tag, tag_prob, intents, data):
                 selected_response = random.choice(responses)
 
                 # Handle specific responses
-                match tag:
-                    # Route
-                    case "route":
-                        return selected_response, "images/Routes.png"
-                    case "route AB":
-                        return selected_response, "images/Route_AB.png"
-                    case "route BA":
-                        return selected_response, "images/Route_BA.png"
-                    case "route C":
-                        return selected_response, "images/Route_C.png"
-                    case "route D":
-                        return selected_response, "images/Route_D.png"
-                    case "route E":
-                        return selected_response, "images/Route_E.png"
-                    case "route 13":
-                        return selected_response, "images/Route_13.png"
-                    # Schedule
-                    case "schedule":
-                        return selected_response, "images/Schedule.png"
-                    case "schedule/frequency for bus AB":
-                        return selected_response, "images/Schedule_AB_BA_E.png"
-                    case "schedule/frequency for bus BA":
-                        return selected_response, "images/Schedule_AB_BA_E.png"
-                    case "schedule/frequency for bus C":
-                        return selected_response, "images/Schedule_C.png"
-                    case "schedule/frequency for bus D":
-                        return selected_response, "images/Schedule_D.png"
-                    case "schedule/frequency for bus E":
-                        return selected_response, "images/Schedule_AB_BA_E.png"
-                    case "schedule/frequency for bus 13":
-                        return selected_response, "images/Schedule_13.png"
-                    # Next and previous bus times
-                    case "next/previous bus for route AB":
-                        return handle_next_previous_bus("AB", data, selected_response)
-                    case "next/previous bus for route BA":
-                        return handle_next_previous_bus("BA", data, selected_response)
-                    case "next/previous bus for route C":
-                        return handle_next_previous_bus("C", data, selected_response)
-                    case "next/previous bus for route D":
-                        return handle_next_previous_bus("D", data, selected_response)
-                    case "next/previous bus for route E":
-                        return handle_next_previous_bus("E", data, selected_response)
-                    case "next/previous bus for route 13":
-                        return handle_next_previous_bus("13", data, selected_response)
-                    # Destinations
-                    case "UM CENTRAL":
-                        return get_routes_for_destination("UM CENTRAL", selected_response, data)
-                    case "KK13":
-                        return get_routes_for_destination("KK13", selected_response, data)
-                    case "KK12":
-                        return get_routes_for_destination("KK12", selected_response, data)
-                    case "KK11":
-                        return get_routes_for_destination("KK11", selected_response, data)
-                    case "KK10":
-                        return get_routes_for_destination("KK10", selected_response, data)
-                    case "KK8":
-                        return get_routes_for_destination("KK8", selected_response, data)
-                    case "KK7":
-                        return get_routes_for_destination("KK7", selected_response, data)
-                    case "KK5":
-                        return get_routes_for_destination("KK5", selected_response, data)
-                    case "KK4":
-                        return get_routes_for_destination("KK4", selected_response, data)
-                    case "KK3":
-                        return get_routes_for_destination("KK3", selected_response, data)
-                    case "KK1":
-                        return get_routes_for_destination("KK1", selected_response, data)
-                    case "INTERNATIONAL HOUSE":
-                        return get_routes_for_destination("INTERNATIONAL HOUSE", selected_response, data)
-                    case "FACULTY OF ENGINEERING":
-                        return get_routes_for_destination("FACULTY OF ENGINEERING", selected_response, data)
-                    case "FACULTY OF SCIENCE":
-                        return get_routes_for_destination("FACULTY OF SCIENCE", selected_response, data)
-                    case "ACADEMY OF ISLAM STUDIES":
-                        return get_routes_for_destination("ACADEMY OF ISLAM STUDIES", selected_response, data)
-                    case "ACADEMY OF MALAY STUDIES":
-                        return get_routes_for_destination("ACADEMY OF MALAY STUDIES", selected_response, data)
-                    case "BANGSAR SOUTH":
-                        return get_routes_for_destination("BANGSAR SOUTH", selected_response, data)
-                    case "KTM ANGKASAPURI STATION":
-                        return get_routes_for_destination("KTM ANGKASAPURI STATION", selected_response, data)
-                    case "PANTAI PERMAI":
-                        return get_routes_for_destination("PANTAI PERMAI", selected_response, data)
-                    case "PASUM":
-                        return get_routes_for_destination("PASUM", selected_response, data)
-                    case "RAPID STOP":
-                        return get_routes_for_destination("RAPID STOP", selected_response, data)
-                   
-                return selected_response, None
+                # Route
+                if tag == "route":
+                    return selected_response, "images/Routes.png"
+                elif tag == "route AB":
+                    return selected_response, "images/Route_AB.png"
+                elif tag == "route BA":
+                    return selected_response, "images/Route_BA.png"
+                elif tag == "route C":
+                    return selected_response, "images/Route_C.png"
+                elif tag == "route D":
+                    return selected_response, "images/Route_D.png"
+                elif tag == "route E":
+                    return selected_response, "images/Route_E.png"
+                elif tag == "route 13":
+                    return selected_response, "images/Route_13.png"
+                # Schedule
+                elif tag == "schedule":
+                    return selected_response, "images/Schedule.png"
+                elif tag == "schedule/frequency for bus AB":
+                    return selected_response, "images/Schedule_AB_BA_E.png"
+                elif tag == "schedule/frequency for bus BA":
+                    return selected_response, "images/Schedule_AB_BA_E.png"
+                elif tag == "schedule/frequency for bus C":
+                    return selected_response, "images/Schedule_C.png"
+                elif tag == "schedule/frequency for bus D":
+                    return selected_response, "images/Schedule_D.png"
+                elif tag == "schedule/frequency for bus E":
+                    return selected_response, "images/Schedule_AB_BA_E.png"
+                elif tag == "schedule/frequency for bus 13":
+                    return selected_response, "images/Schedule_13.png"
+                # Next and previous bus times
+                elif tag == "next/previous bus for route AB":
+                    return handle_next_previous_bus("AB", data, selected_response)
+                elif tag == "next/previous bus for route BA":
+                    return handle_next_previous_bus("BA", data, selected_response)
+                elif tag == "next/previous bus for route C":
+                    return handle_next_previous_bus("C", data, selected_response)
+                elif tag == "next/previous bus for route D":
+                    return handle_next_previous_bus("D", data, selected_response)
+                elif tag == "next/previous bus for route E":
+                    return handle_next_previous_bus("E", data, selected_response)
+                elif tag == "next/previous bus for route 13":
+                    return handle_next_previous_bus("13", data, selected_response)
+                # Destinations
+                elif tag == "UM CENTRAL":
+                    return get_routes_for_destination("UM CENTRAL", selected_response, data)
+                elif tag == "KK13":
+                    return get_routes_for_destination("KK13", selected_response, data)
+                elif tag == "KK12":
+                    return get_routes_for_destination("KK12", selected_response, data)
+                elif tag == "KK11":
+                    return get_routes_for_destination("KK11", selected_response, data)
+                elif tag == "KK10":
+                    return get_routes_for_destination("KK10", selected_response, data)
+                elif tag == "KK8":
+                    return get_routes_for_destination("KK8", selected_response, data)
+                elif tag == "KK7":
+                    return get_routes_for_destination("KK7", selected_response, data)
+                elif tag == "KK5":
+                    return get_routes_for_destination("KK5", selected_response, data)
+                elif tag == "KK4":
+                    return get_routes_for_destination("KK4", selected_response, data)
+                elif tag == "KK3":
+                    return get_routes_for_destination("KK3", selected_response, data)
+                elif tag == "KK1":
+                    return get_routes_for_destination("KK1", selected_response, data)
+                elif tag == "INTERNATIONAL HOUSE":
+                    return get_routes_for_destination("INTERNATIONAL HOUSE", selected_response, data)
+                elif tag == "FACULTY OF ENGINEERING":
+                    return get_routes_for_destination("FACULTY OF ENGINEERING", selected_response, data)
+                elif tag == "FACULTY OF SCIENCE":
+                    return get_routes_for_destination("FACULTY OF SCIENCE", selected_response, data)
+                elif tag == "ACADEMY OF ISLAM STUDIES":
+                    return get_routes_for_destination("ACADEMY OF ISLAM STUDIES", selected_response, data)
+                elif tag == "ACADEMY OF MALAY STUDIES":
+                    return get_routes_for_destination("ACADEMY OF MALAY STUDIES", selected_response, data)
+                elif tag == "BANGSAR SOUTH":
+                    return get_routes_for_destination("BANGSAR SOUTH", selected_response, data)
+                elif tag == "KTM ANGKASAPURI STATION":
+                    return get_routes_for_destination("KTM ANGKASAPURI STATION", selected_response, data)
+                elif tag == "PANTAI PERMAI":
+                    return get_routes_for_destination("PANTAI PERMAI", selected_response, data)
+                elif tag == "PASUM":
+                    return get_routes_for_destination("PASUM", selected_response, data)
+                elif tag == "RAPID STOP":
+                    return get_routes_for_destination("RAPID STOP", selected_response, data)
+                else:
+                    return selected_response, None
                 
         return "I'm sorry, I don't understand. Please try a different question.", None
     else:
